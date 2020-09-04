752647a3e6bbb1d2e3cfc54f7eb1cf2d57b987a8
andykwanfai <andykwanfai@gmail.com>
2020-09-04 16:31:17 +0900

    v0.1

--- index.js
+++ index.js
@@ -11,65 +11,122 @@ const folder = `showroom-${Date.now()}/`
 
 let path = `${dir}${folder}`
 
-
+const pastStream = 50
+const retry = 5
 const success = new Set()
-const fail = new Set()
+const failed = new Set()
+const cur_fail = {}
 
-let start
+let startIndex
 
 async function download(url) {
+  // try {
   const res = await axios({
     method: 'get',
     url: url,
-    timeout: 5000
+    timeout: 5000,
+    responseEncoding: 'binary'
   })
+
   return res.data
+  // } catch (err) {
+  //   console.log("down error")
+  // }
 }
 
-handleError = (err, filename) => {
-  fail.add(filename)
+saveStream = (stream, filename) => {
+  fs.writeFile(`${path}${filename}`, stream, { encoding: "binary" }, (err) => {
+    if (err) {
+      handleError(err, filename)
+    } else {
+      success.add(filename)
+      console.log(`saved ${filename}`)
+    }
+  })
+}
+
+handleError = (err, filename, index) => {
+  if (index > startIndex - pastStream) {
+    if (cur_fail[index]) {
+      cur_fail[index] = cur_fail[index] + 1
+    } else {
+      cur_fail[index] = 1
+    }
+  }
   console.log(err.message)
   console.log(`failed download ${filename}`)
 }
 
-async function getStream(filename) {
-  // setTimeout(async () => {
+//get m3u8 and return stream list
+async function getStreamList() {
   try {
-    const stream = await download(url + filename)
-    fs.writeFile(`${path}${filename}`, stream, (err) => {
-      if (err) {
-        handleError(err, filename)
-      } else {
-        success.add(filename)
-        console.log(`downloaded ${filename}`)
-      }
-    })
+    const m3u8 = await download(m3u8_url)
+    return m3u8.match(/media_\d+.ts/g)
   } catch (err) {
-    handleError(err, filename)
+    console.log("cannot get m3u8 file")
   }
-
-  // }, 1000);
 }
 
-getStreamList = (m3u8) => {
-  const list = m3u8.match(/media_\d+.ts/g)
-  return list
-}
-
-
-async function oldStreamRecover() {
-
+//try to download past 30 streams
+async function oldStreamRecover(start) {
+  for (let i = start - pastStream > 0 ? start - pastStream : 1; i < start; i++) {
+    const filename = `media_${i}.ts`
+    try {
+      const stream = await download(url + filename)
+      saveStream(stream, filename)
+      console.log(`Recover ${filename}`)
+    } catch (err) {
+      handleError(err, filename, i)
+      // console.log("fail recover" + filename)
+    }
+  }
 }
 
 async function failureRecover() {
-
+  for (const [key, value] of Object.entries(cur_fail)) {
+    const filename = `media_${key}.ts`
+    try {
+      const stream = await download(url + filename)
+      saveStream(stream, filename)
+      delete cur_fail[key]
+    } catch (err) {
+      cur_fail[key] = value + 1
+      if (value + 1 > retry) {
+        if (key >= startIndex) {
+          failed.add(filename)
+        }
+        delete cur_fail[key]
+      }
+    }
+  }
+  console.log(cur_fail)
+  console.log(failed)
 }
 
 async function downloadStream() {
+  try {
+    const streamList = await getStreamList()
+    streamList.forEach(async filename => {
+      if (!success.has(filename)) {
+        try {
+          const stream = await download(url + filename)
+          saveStream(stream, filename)
+        } catch (err) {
+          handleError(err, filename)
+        }
+      }
+    });
+  } catch (err) {
+    console.log("cannot download stream")
+  }
+}
+
+function logError() {
 
 }
 
-function main() {
+async function main() {
+  //create folder
   try {
     fs.mkdirSync(path)
   } catch (err) {
@@ -78,26 +135,18 @@ function main() {
     // fs.mkdirSync(folder)
     // path = folder
   }
-  setInterval(async () => {
-    let m3u8
-    try {
-      m3u8 = await download(m3u8_url)
-    } catch (err) {
-      console.log(err.message)
-      return
-    }
-    const fileList = getStreamList(m3u8)
 
-    if (!start) {
-      start = fileList[0].match(/\d+/g)[0]
-    }
-    // console.log(start)
-    fileList.forEach(filename => {
-      if (!success.has(filename)) {
-        getStream(filename)
-      }
-    });
-  }, 1500);
+  try {
+    const streamList = await getStreamList()
+    startIndex = streamList[0].match(/\d+/g)[0]
+  } catch (err) {
+    console.log(err.message)
+    console.log("Cannot recover old streams")
+  }
+  console.log("start: " + startIndex)
+  oldStreamRecover(startIndex)
+  setInterval(failureRecover, 200);
+  setInterval(downloadStream, 1500);
 }
 
 
